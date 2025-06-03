3e191261d11a3ba673e9dc2a8eb513e7b4857126
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-03 22:17:28 +0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Gyms from "./pages/Gyms";
 
 export default function AppRoutes() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gyms" element={<Gyms />} />
+        {/* Add more routes here later */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
