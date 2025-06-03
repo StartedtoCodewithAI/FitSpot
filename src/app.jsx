03bf175295dba6bafb5bbbe4790f6dcec73c5de6
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-03 21:50:13 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,15 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Gyms from "./pages/Gyms";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Gyms from './pages/Gyms.jsx';
 
 export default function App() {
   return (
-    <div>
-      <nav style={{ padding: "1rem", backgroundColor: "#007bff", color: "#fff" }}>
-        <Link to="/" style={{ marginRight: "1rem", color: "#fff" }}>Home</Link>
-        <Link to="/gyms" style={{ color: "#fff" }}>Gyms</Link>
-      </nav>
-
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gyms" element={<Gyms />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
