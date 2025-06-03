46b3561e244ccff0c4266eed13ef5f62310867d2
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-03 21:47:54 +0300

    Create Gyms.jsx

---
+++ src/pages/Gyms.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Gyms() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Available Gyms</h1>
+      <p>List of gyms will go here.</p>
+    </div>
+  );
+}
