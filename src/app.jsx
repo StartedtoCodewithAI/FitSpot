3cae473f1a78c60bbc1d8b779a7c39c071ffaeef
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-03 23:19:41 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Gyms from "./pages/Gyms";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gyms" element={<Gyms />} />
-      </Routes>
+      <div style={{ padding: "1rem" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gyms" element={<Gyms />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
