9267499116f62b869aa5307a9176959b4b6791ec
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-03 22:22:59 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,18 @@
 import React from "react";
-import AppRoutes from "./routes";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Gyms from "./pages/Gyms";
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/gyms">Gyms</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gyms" element={<Gyms />} />
+      </Routes>
+    </Router>
+  );
 }
