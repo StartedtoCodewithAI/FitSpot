cb1a2bef1d6fe82385de92caa51f3c174509d391
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-03 22:17:45 +0300

    Update Gyms.jsx

--- src/pages/Gyms.jsx
+++ src/pages/Gyms.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export default function Gyms() {
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Available Gyms</h1>
-      <p>List of gyms will go here.</p>
+      <h1>Nearby Gyms</h1>
+      <p>This will list gyms near the user.</p>
+      {/* You can later add gym cards, maps, filters, etc. */}
     </div>
   );
 }
