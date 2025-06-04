2dc4a9f5092ce06bb6e5600c0fc37d8974b4430e
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-04 21:27:44 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FitSpotLogo from "../FitSpot.png";
 
 export default function Navbar() {
   return (
-    <nav style={navStyle}>
-      <ul style={ulStyle}>
-        <li style={liStyle}>
-          <Link to="/" style={linkStyle}>Home</Link>
-        </li>
-        <li style={liStyle}>
-          <Link to="/gyms" style={linkStyle}>Gyms</Link>
-        </li>
-        <li style={liStyle}>
-          <Link to="/profile" style={linkStyle}>Profile</Link>
-        </li>
-      </ul>
+    <nav style={{ display: "flex", padding: "1rem", alignItems: "center", background: "#eee" }}>
+      <img src={FitSpotLogo} alt="FitSpot Logo" style={{ height: "40px", marginRight: "1rem" }} />
+      <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
+      <Link to="/gyms" style={{ marginRight: "1rem" }}>Gyms</Link>
+      <Link to="/profile">Profile</Link>
     </nav>
   );
 }
-
-const navStyle = {
-  padding: "1rem",
-  backgroundColor: "#007bff",
-};
-
-const ulStyle = {
-  display: "flex",
-  listStyle: "none",
-  margin: 0,
-  padding: 0,
-  justifyContent: "center",
-  gap: "2rem",
-};
-
-const liStyle = {};
-
-const linkStyle = {
-  color: "white",
-  textDecoration: "none",
-  fontWeight: "bold",
-  fontSize: "1.2rem",
-};
