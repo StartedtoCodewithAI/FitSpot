35114cfe453b30812e9dfedf1c5c8a7bf28b81da
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-04 22:35:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'public',  // <--- tell Vite where to find index.html
   plugins: [react()],
   build: {
-    outDir: '../dist' // output relative to 'public'
+    outDir: 'dist'
   }
 })
