8de20b7a505c94b6a527b75cbd5550d137984b58
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-04 22:36:38 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Gyms from "./pages/Gyms";
-import Profile from "./pages/profile";
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/gyms" element={<Gyms />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
-  );
+    <div>
+      <h1>Welcome to FitSpot</h1>
+      {/* Your existing app content */}
+    </div>
+  )
 }
-
-export default App;
