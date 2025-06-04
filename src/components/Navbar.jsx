ef45e310705778fafea8468bff1bbf85dc4dc147
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-04 23:33:05 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FitSpotLogo from '../assets/FitSpot.png';
 
 function Navbar() {
   return (
-    <nav>
-      <img src="/src/assets/FitSpot.png" alt="FitSpot Logo" />
-      {/* other nav code */}
+    <nav style={{ display: 'flex', alignItems: 'center', padding: '10px', backgroundColor: '#f8f8f8' }}>
+      <img src={FitSpotLogo} alt="FitSpot Logo" style={{ height: '40px', marginRight: '20px' }} />
+      <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
+        <li style={{ marginRight: '15px' }}>
+          <Link to="/">Home</Link>
+        </li>
+        <li style={{ marginRight: '15px' }}>
+          <Link to="/gyms">Gyms</Link>
+        </li>
+        <li style={{ marginRight: '15px' }}>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
