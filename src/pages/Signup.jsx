42bbe2a4cd48e32dbacc72447bd876b9db60a94c
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-11 21:56:03 +0300

    Create Signup.jsx

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+// src/pages/Signup.jsx
+import React from "react";
+export default function Signup() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Sign Up</h1>
+      <p>This is the signup page. Add your signup form here.</p>
+    </div>
+  );
+}
