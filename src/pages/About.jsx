1fa8e40c4bddb8e7df8e563c21e70c6e8d8bac7e
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-11 21:56:26 +0300

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+// src/pages/About.jsx
+import React from "react";
+export default function About() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>About FitSpot</h1>
+      <p>
+        FitSpot helps you find and access nearby gyms with daily passes. 
+        More features and info coming soon!
+      </p>
+    </div>
+  );
+}
