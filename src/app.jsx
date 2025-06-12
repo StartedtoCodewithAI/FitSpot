f94e2e16a1665cb27d4a49077285b77da4e7f67d
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-12 23:49:37 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Gyms from './pages/Gyms';
-import Profile from './pages/Profile'; // <-- Capital "P"
+import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import About from './pages/About';
@@ -12,6 +12,14 @@ import PrivateRoute from './components/PrivateRoute';
 export default function App() {
   return (
     <Router>
+      {/* Prevent horizontal overflow and white line on mobile */}
+      <style>{`
+        html, body, #root {
+          width: 100%;
+          max-width: 100vw;
+          overflow-x: hidden;
+        }
+      `}</style>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
