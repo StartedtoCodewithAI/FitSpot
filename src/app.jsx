c64b0288a6e293e596ddb889434d5ed7b3a0e1b2
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-14 00:40:04 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -9,37 +9,25 @@ import Login from './pages/Login';
 import About from './pages/About';
 import MyCodes from './pages/MyCodes';
 import PrivateRoute from './components/PrivateRoute';
-// NEW IMPORT
 import BookSession from './pages/BookSession';
 
 export default function App() {
   return (
     <Router>
-      {/* Prevent horizontal overflow and white line on mobile */}
-      <style>{`
-        html, body, #root {
-          width: 100%;
-          max-width: 100vw;
-          overflow-x: hidden;
-        }
-      `}</style>
-      <Navbar />
+      {/* ...styles and Navbar... */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gyms" element={<Gyms />} />
         <Route path="/my-codes" element={<MyCodes />} />
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          }
-        />
+        {/* ...other routes... */}
+        <Route path="/profile" element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        }/>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
-        {/* NEW ROUTE for the booking flow */}
         <Route path="/book/:gymId" element={<BookSession />} />
       </Routes>
     </Router>
