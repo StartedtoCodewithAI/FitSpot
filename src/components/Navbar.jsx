c6ce050c766d1eefb57dd977a1c955774a9d1b77
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-15 00:12:25 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// Add this import if you have an AvatarUpload component
+import AvatarUpload from "./AvatarUpload";
 
 export default function Navbar() {
   return (
@@ -13,11 +15,13 @@ export default function Navbar() {
       <div style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
         <Link to="/" style={{ textDecoration: "none", color: "black" }}>FitSpot</Link>
       </div>
-      <div style={{ display: "flex", gap: "1rem" }}>
+      <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
         <Link to="/">Home</Link>
         <Link to="/gyms">Gyms</Link>
         <Link to="/about">About</Link>
         <Link to="/profile">Profile</Link>
+        {/* Add avatar/profile upload here */}
+        <AvatarUpload />
       </div>
     </nav>
   );
