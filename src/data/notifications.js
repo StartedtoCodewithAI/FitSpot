eff99f039d7f8d13daf5f6517c75aa3eadccb3d1
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-15 01:40:02 +0300

    Create notifications.js

---
+++ src/data/notifications.js
@@ -0,0 +1,6 @@
+// src/data/notifications.js
+const notifications = [
+  { id: 1, text: "Welcome to FitSpot!" },
+  { id: 2, text: "Don't forget to book your next session!" }
+];
+export default notifications;
