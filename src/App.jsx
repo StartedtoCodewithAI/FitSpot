bf0e7425b66f6d891c1ba3d9b6b977368fe79ea0
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-15 00:07:24 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { HashRouter as Router } from "react-router-dom";
 import Navbar from './components/Navbar';
 
 export default function App() {
-  return <Navbar />;
+  return (
+    <Router>
+      <Navbar />
+      <div>It works!</div>
+    </Router>
+  );
 }
