76fe7f627b115fdf9d9cfc965b1042eab90a3a8c
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-15 00:13:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Home from './pages/Home';
 import Gyms from './pages/Gyms';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import BookSession from './pages/BookSession';
+import MyCodes from './pages/MyCodes';
 
 export default function App() {
   return (
@@ -15,6 +19,10 @@ export default function App() {
         <Route path="/gyms" element={<Gyms />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/book-session" element={<BookSession />} />
+        <Route path="/my-codes" element={<MyCodes />} />
       </Routes>
     </Router>
   );
