abba07d1272ce7bb0ad42bdd3e17eb6b84956190
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-15 00:10:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Gyms from './pages/Gyms';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
     <Router>
       <Navbar />
-      <div>It works!</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gyms" element={<Gyms />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </Router>
   );
 }
