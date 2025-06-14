3612e1f9ebea2e21c463fd7bab3c3c574c0a79ed
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-15 00:03:38 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{background: "#eee", padding: "1rem", display: "flex", gap: "1rem"}}>
-      <Link to="/">Home</Link>
-      <Link to="/gyms">Gyms</Link>
-      <Link to="/about">About</Link>
-      <Link to="/profile">Profile</Link>
+    <nav style={{
+      background: "#eee",
+      padding: "1rem",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center"
+    }}>
+      <div style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+        <Link to="/" style={{ textDecoration: "none", color: "black" }}>FitSpot</Link>
+      </div>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <Link to="/">Home</Link>
+        <Link to="/gyms">Gyms</Link>
+        <Link to="/about">About</Link>
+        <Link to="/profile">Profile</Link>
+      </div>
     </nav>
   );
 }
