d678adf0ae6b1190f6bb49848581c5b9ea452e10
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-15 00:00:53 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-  return <div style={{background: "#eee", padding: "1rem"}}>Navbar works!</div>;
+  return (
+    <nav style={{background: "#eee", padding: "1rem", display: "flex", gap: "1rem"}}>
+      <Link to="/">Home</Link>
+      <Link to="/gyms">Gyms</Link>
+      <Link to="/about">About</Link>
+      <Link to="/profile">Profile</Link>
+    </nav>
+  );
 }
