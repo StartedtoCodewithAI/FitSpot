28e75397b8b70436b819e865147017f100858679
StartedtoCodewithAI <a.nikolopoulos1@hotmail.com>
2025-06-16 00:11:43 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";More actions
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import fitspotLogo from "../assets/FitSpot.png";
 import { supabase } from "../supabaseClient";
